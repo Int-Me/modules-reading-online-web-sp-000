6174c3227672420f03d961ab474faf02e31bc998
Roland McRae <mcraeroland@gmail.com>
2020-05-15 05:09:15 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,5 +1,6 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
 
 class Kid
   include Dance
@@ -10,4 +11,6 @@ class Kid
     @name = name
   end
   extend MetaDancing
+  extend FancyDance::ClassMethods
+include FancyDance::InstanceMethods
 end
